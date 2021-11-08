ee5f956963ed86005ceb3f9aadd2f1a5f0213f9d
semarslan <94.sema.arslan@gmail.com>
2021-11-08 10:48:29 +0300

    First commit

---
+++ src/main/java/com/semarslan/grpcapp/GrpcAppApplication.java
@@ -0,0 +1,13 @@
+package com.semarslan.grpcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcAppApplication.class, args);
+	}
+
+}
